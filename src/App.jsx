8ef7f2d433af783fc7f9898200f25e3fc5535284
RustamZamanov1986 <160174660+RustamZamanov1986@users.noreply.github.com>
2024-11-13 16:43:02 +0100

    added a new structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import ProfileCard from "./components/ProfileCard/ProfileCard.jsx";
-import "./components/ProfileCard/styles.css";
+import "./App.css";
+
+//lesson imports
+//import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./lessons/Lesson03/Lesson03";
+
+//homework imports
+//import Homework02 from "./homeworks/Homework02/Homework02";
+
+//consultation imports
+
 
 function App() {
   return (
-    <div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+    <div className="App">
+      {/* <Lesson02 /> */}
+      {/* <Homework02 /> */}
+      <Lesson03/>
     </div>
   );
 }
